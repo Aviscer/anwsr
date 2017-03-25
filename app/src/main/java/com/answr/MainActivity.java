c6f697ac21e4a04dba435426c74467dc19294efd
Aviscer <aviscerality@gmail.com>
2017-03-24 22:49:21 -0700

    Got firebase working

--- app/src/main/java/com/answr/MainActivity.java
+++ app/src/main/java/com/answr/MainActivity.java
@@ -2,12 +2,65 @@ package com.answr;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView mBoolTextView;
+    Button mButtonTrue;
+    Button mButtonFalse;
+
+    DatabaseReference mRootRef = FirebaseDatabase.getInstance().getReference();
+    DatabaseReference mBoolRef = mRootRef.child("bool");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // UI elements
+        mBoolTextView = (TextView) findViewById(R.id.textViewBool);
+        mButtonTrue = (Button) findViewById(R.id.buttonTrue);
+        mButtonFalse = (Button) findViewById(R.id.buttonFalse);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        mBoolRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String text = dataSnapshot.getValue(String.class);
+                mBoolTextView.setText(text);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+        mButtonTrue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mBoolRef.setValue("Poop");
+            }
+        });
+
+        mButtonFalse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mBoolRef.setValue("No poop");
+            }
+        });
     }
 }
